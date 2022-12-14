569e77fc5f755a0e60944224c0b00cd2232ea5a7
Sam Heller <sam@faitaccomp.li>
2022-12-14 10:37:38 -0500

    Nevermind

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    target: 'webworker',
-    entry: './index.js',
-    mode: 'production',
-    externals: {
-        "itty-router": "itty-router",
-        "itty-router-extras": "itty-router-extras"
-    }
-};
